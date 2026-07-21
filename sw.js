a17d190aa2310ea75b23d38b0c6ae5c30dbcac59
rahatulyerbolat05-dotcom <rahatulyerbolat05@gmail.com>
2026-07-21 21:20:33 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,9 +3,10 @@
    - иконки/манифест/фотообои → сначала кэш (не меняются)
    - запросы к script.google.com не трогаем вовсе (синхронизация)
 */
-var CACHE='qalta-v1';
+var CACHE='qalta-v2';
 var PRECACHE=['./','./index.html','./manifest.json','./icon-180.png','./icon-512.png',
-  './wp_manor.jpg','./wp_lagoon.jpg','./wp_dusk.jpg','./wp_vangogh.jpg','./wp_barkhan.jpg','./wp_mirage.jpg','./wp_bloom.jpg'];
+  './wp_manor.jpg','./wp_lagoon.jpg','./wp_dusk.jpg','./wp_vangogh.jpg','./wp_barkhan.jpg','./wp_mirage.jpg','./wp_bloom.jpg',
+  './ic_dark.png','./ic_navy.png','./ic_emerald.png','./ic_velvet.png','./ic_lime.png','./ic_amethyst.png','./ic_ocean.png','./ic_gold.png','./ic_onyx.png','./ic_light.png','./ic_sepia.png','./ic_porcelain.png','./ic_rose.png','./ic_marble.png','./ic_dune.png','./ic_aura.png','./ic_orange.png','./ic_pastel.png','./ic_wp-mirage.png','./ic_wp-dusk.png','./ic_wp-barkhan.png','./ic_wp-lagoon.png','./ic_wp-vangogh.png','./ic_wp-bloom.png','./ic_wp-manor.png'];
 
 self.addEventListener('install',function(e){
   e.waitUntil(
